1539d32c5f9a5006a11f068f485aa752d32c683e
Aleksandr Kotishevskii <kotishevskiyaa@gmail.com>
2022-12-14 13:56:32 +0300

    Puzzle solved

--- Program.cs
+++ Program.cs
@@ -1,2 +1,31 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Начало программы");
+
+int distance = 10000;
+int firstFriendSpeed = 1;
+int secondFriendSpeed = 2;
+int dogSpeed = 5;
+int friend = 2;
+
+int time = 0;
+int count = 0;
+
+while (distance > 10)
+{
+    if (friend == 1)
+    {
+        time = distance / (firstFriendSpeed + dogSpeed);
+        friend = 2;
+    }
+    else
+    {
+        time = distance / (secondFriendSpeed + dogSpeed);
+        friend = 1;
+    }
+    distance = distance - (firstFriendSpeed + secondFriendSpeed) * time;
+    count++;
+}
+
+    Console.Write("Собака пробежит ");
+    Console.Write(count);
+    Console.WriteLine(" раз");
